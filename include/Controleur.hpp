16259f23001727b4bbd442253f02c6be70f85bd8
Louis Vaillant <louis.vaillant@eleves.ec-nantes.fr>
2026-01-12 17:33:37 +0100

    [Dev] (Q2.1) Implementation of Controleur class

--- include/Controleur.hpp
+++ include/Controleur.hpp
@@ -1,38 +1,60 @@
 #ifndef CONTROLEUR_HPP
 #define CONTROLEUR_HPP
 
-#include <thread>             
+#include <thread>
 #include <iostream>
 #include <mutex>
 #include <condition_variable>
 
-using namespace std; 
+using namespace std;
 
-class Controleur {
-  public:
-    Controleur (int valeur_initiale) : 
-    val(valeur_initiale)
+class Controleur
+{
+public:
+  Controleur(int valeur_initiale) : train_counter(valeur_initiale)
+  {
+  }
+
+  bool controlinEnB(int numero)
+  {
+    if (train_counter >= 0)
     {
-    }
-    
-    bool controlinEnB(int numero) { 
-        return true;
-    }  
-
-    bool controlinEnA(int numero) { 
-        return true;
-    }  
-
-    bool controloutEnB(int numero){ 
-      return true ;
-    }
-
-    bool controloutEnA(int numero){ 
+      train_counter++;
       return true;
     }
+    return false;
+  }
 
-  private:
-    int val;
+  bool controlinEnA(int numero)
+  {
+    if (train_counter <= 0)
+    {
+      train_counter--;
+      return true;
+    }
+    return false;
+  }
+
+  bool controloutEnB(int numero)
+  {
+    train_counter++;
+    return true;
+  }
+
+  bool controloutEnA(int numero)
+  {
+    train_counter--;
+    return true;
+  }
+
+private:
+  /**
+   * @brief Compteur de trains
+   * @details Négatif = trains circulant de A à B
+   *          Positif = trains circulant de B à A
+   *          0 = Aucun train ne circule
+   */
+  int train_counter;
 };
 
-#endif //CONTROLEUR_HPP
+#endif // CONTROLEUR_HPP
